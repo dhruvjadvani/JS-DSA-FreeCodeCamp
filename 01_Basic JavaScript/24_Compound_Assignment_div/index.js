be548c05ef1b3d87eff7f1ef2018a1e2b7e9a78b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 18:28:57 +0530

    Update index.js

--- 01_Basic JavaScript/24_Compound_Assignment_div/index.js
+++ 01_Basic JavaScript/24_Compound_Assignment_div/index.js
@@ -1,1 +1,10 @@
+let a = 48;
+let b = 108;
+let c = 33;
 
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-division
