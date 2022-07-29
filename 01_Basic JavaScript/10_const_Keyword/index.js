7a43577467e0722dab69376470b3752d7bbc6ba4
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 11:13:56 +0530

    Update index.js

--- 01_Basic JavaScript/10_const_Keyword/index.js
+++ 01_Basic JavaScript/10_const_Keyword/index.js
@@ -2,3 +2,5 @@ const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-a-read-only-variable-with-the-const-keyword
