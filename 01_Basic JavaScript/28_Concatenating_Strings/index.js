77d3771bf88f6c36b232ca81590b6eec04340466
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 21:23:15 +0530

    Update index.js

--- 01_Basic JavaScript/28_Concatenating_Strings/index.js
+++ 01_Basic JavaScript/28_Concatenating_Strings/index.js
@@ -1,1 +1,3 @@
+const myStr = "This is the start." + " This is the end."; // Change this line
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/concatenating-strings-with-plus-operator
