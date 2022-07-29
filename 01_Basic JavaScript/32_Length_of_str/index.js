3d21c3f435082505a0ec7c255b2feb0900d7c456
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 21:36:07 +0530

    Update index.js

--- 01_Basic JavaScript/32_Length_of_str/index.js
+++ 01_Basic JavaScript/32_Length_of_str/index.js
@@ -1,1 +1,8 @@
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
 
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/find-the-length-of-a-string
