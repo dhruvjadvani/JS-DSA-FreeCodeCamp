90178c70ef845581f3948e5703e1a8f644e428d8
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 11:27:07 +0530

    Update index.js

--- 01_Basic JavaScript/19_Divide_One_Decimal_by_Another/index.js
+++ 01_Basic JavaScript/19_Divide_One_Decimal_by_Another/index.js
@@ -1,1 +1,3 @@
+const quotient = 4.4 / 2.0; // Change this line
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/divide-one-decimal-by-another-with-javascript
