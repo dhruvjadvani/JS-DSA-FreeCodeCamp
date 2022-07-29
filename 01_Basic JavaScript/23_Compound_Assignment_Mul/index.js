cfc8c895888110359416249e72b9d997ca50a7b4
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 18:06:48 +0530

    Update index.js

--- 01_Basic JavaScript/23_Compound_Assignment_Mul/index.js
+++ 01_Basic JavaScript/23_Compound_Assignment_Mul/index.js
@@ -1,1 +1,11 @@
+let a = 5;
+let b = 12;
+let c = 4.6;
 
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-multiplication
