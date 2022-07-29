96798217bcab855ada959a82ddb8907f65c05c82
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 11:24:50 +0530

    Update index.js

--- 01_Basic JavaScript/17_Create_Decimal_Num/index.js
+++ 01_Basic JavaScript/17_Create_Decimal_Num/index.js
@@ -2,3 +2,5 @@ const ourDecimal = 5.7;
 
 // Only change code below this line
 let myDecimal = 1.2;
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/create-decimal-numbers-with-javascript
