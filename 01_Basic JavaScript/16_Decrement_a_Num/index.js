359bb61d72db357acd7d8b48091e600b333b3bae
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 11:24:25 +0530

    Update index.js

--- 01_Basic JavaScript/16_Decrement_a_Num/index.js
+++ 01_Basic JavaScript/16_Decrement_a_Num/index.js
@@ -2,3 +2,5 @@ let myVar = 11;
 
 // Only change code below this line
 myVar = --myVar;
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/decrement-a-number-with-javascript
