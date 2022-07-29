5e80ccfbdc06f60181c83a0977adfaa6e20707f4
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 11:13:21 +0530

    Update index.js

--- 01_Basic JavaScript/09_diff_bw_var_n_let/index.js
+++ 01_Basic JavaScript/09_diff_bw_var_n_let/index.js
@@ -1,2 +1,4 @@
 let catName = "Oliver";
 let catSound = "Meow!";
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/explore-differences-between-the-var-and-let-keywords
