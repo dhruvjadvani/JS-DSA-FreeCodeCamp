cf3d0aa2bbc02d3b3e3cc9382dfdf2265c78f50e
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 21:18:09 +0530

    Update index.js

--- 01_Basic JavaScript/27_Escape_Sequences_in_Strings/index.js
+++ 01_Basic JavaScript/27_Escape_Sequences_in_Strings/index.js
@@ -1,1 +1,3 @@
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/escape-sequences-in-strings
