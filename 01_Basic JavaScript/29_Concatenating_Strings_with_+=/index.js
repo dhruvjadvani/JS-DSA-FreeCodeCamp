8663f025d0caf08c4fd56f28185d357e50434778
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 21:26:02 +0530

    Update index.js

--- 01_Basic JavaScript/29_Concatenating_Strings_with_+=/index.js
+++ 01_Basic JavaScript/29_Concatenating_Strings_with_+=/index.js
@@ -1,1 +1,4 @@
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/concatenating-strings-with-the-plus-equals-operator
