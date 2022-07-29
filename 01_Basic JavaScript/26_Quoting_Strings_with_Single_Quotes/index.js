7e6f9b2ffbc984e4708e3304b3b1f6ed43f3441a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 21:10:16 +0530

    Update index.js

--- 01_Basic JavaScript/26_Quoting_Strings_with_Single_Quotes/index.js
+++ 01_Basic JavaScript/26_Quoting_Strings_with_Single_Quotes/index.js
@@ -1,1 +1,3 @@
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/quoting-strings-with-single-quotes
