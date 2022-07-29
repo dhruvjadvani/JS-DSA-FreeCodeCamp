313692464dacea9022c3d4fff7ca07390c041113
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 21:34:25 +0530

    Update index.js

--- 01_Basic JavaScript/31_Appending_Variables_to_str/index.js
+++ 01_Basic JavaScript/31_Appending_Variables_to_str/index.js
@@ -1,1 +1,6 @@
+// Change code below this line
+const someAdjective = "fun";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/appending-variables-to-strings
