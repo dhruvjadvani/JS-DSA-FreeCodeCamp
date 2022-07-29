88dbcf7b81559f6a6d415a6d3bda1d327877ee64
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 18:02:27 +0530

    Update index.js

--- 01_Basic JavaScript/22_Compound_Assignment_sub/index.js
+++ 01_Basic JavaScript/22_Compound_Assignment_sub/index.js
@@ -1,1 +1,10 @@
+let a = 11;
+let b = 9;
+let c = 3;
 
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-subtraction
