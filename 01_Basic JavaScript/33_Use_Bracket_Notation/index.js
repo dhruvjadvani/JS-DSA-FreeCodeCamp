6571813a4b1151a2a5288a01d4a221c29db79a1b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 22:47:33 +0530

    Update index.js

--- 01_Basic JavaScript/33_Use_Bracket_Notation/index.js
+++ 01_Basic JavaScript/33_Use_Bracket_Notation/index.js
@@ -1,1 +1,8 @@
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
 
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-bracket-notation-to-find-the-first-character-in-a-string
