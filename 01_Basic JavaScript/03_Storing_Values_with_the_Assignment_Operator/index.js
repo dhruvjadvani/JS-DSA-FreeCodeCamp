b1a83eab16abc962f0267a4b83c24b6f0af671cb
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 11:09:40 +0530

    Update index.js

--- 01_Basic JavaScript/03_Storing_Values_with_the_Assignment_Operator/index.js
+++ 01_Basic JavaScript/03_Storing_Values_with_the_Assignment_Operator/index.js
@@ -3,3 +3,5 @@ var a;
 
 // Only change code below this line
 a = 7;
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/storing-values-with-the-assignment-operator
