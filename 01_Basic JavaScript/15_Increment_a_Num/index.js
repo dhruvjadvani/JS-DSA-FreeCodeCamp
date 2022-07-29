41c72c09e912df812f661cb9b7e7f07bce0cf22f
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 11:23:59 +0530

    Update index.js

--- 01_Basic JavaScript/15_Increment_a_Num/index.js
+++ 01_Basic JavaScript/15_Increment_a_Num/index.js
@@ -2,3 +2,5 @@ let myVar = 87;
 
 // Only change code below this line
 myVar = ++myVar;
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/increment-a-number-with-javascript
