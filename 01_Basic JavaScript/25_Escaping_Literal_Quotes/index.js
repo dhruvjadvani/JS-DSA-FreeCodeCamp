53af55c22735e5dc4bc1b46376e72ede97f7ddd6
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 18:31:50 +0530

    Update index.js

--- 01_Basic JavaScript/25_Escaping_Literal_Quotes/index.js
+++ 01_Basic JavaScript/25_Escaping_Literal_Quotes/index.js
@@ -1,1 +1,3 @@
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/escaping-literal-quotes-in-strings
