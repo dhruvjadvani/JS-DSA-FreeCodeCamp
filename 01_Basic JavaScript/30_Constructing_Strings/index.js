991d8cb2c33d9d7833dcf7ae725472f72b31f24b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-29 21:32:38 +0530

    Update index.js

--- 01_Basic JavaScript/30_Constructing_Strings/index.js
+++ 01_Basic JavaScript/30_Constructing_Strings/index.js
@@ -1,1 +1,6 @@
+// Only change code below this line
+const myName = "Dhruv";
+const myStr = "My name is " + myName + " Jadvani";
 
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/constructing-strings-with-variables
