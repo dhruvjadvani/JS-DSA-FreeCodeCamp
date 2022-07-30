913ddc8c6eb6414d7ff5a3caf722865a0356180f
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-30 14:22:29 +0530

    Update index.js

--- 01_Basic JavaScript/36_Find_the_Last_Character/index.js
+++ 01_Basic JavaScript/36_Find_the_Last_Character/index.js
@@ -1,1 +1,7 @@
+// Setup
+const lastName = "Lovelace";
 
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-bracket-notation-to-find-the-last-character-in-a-string
