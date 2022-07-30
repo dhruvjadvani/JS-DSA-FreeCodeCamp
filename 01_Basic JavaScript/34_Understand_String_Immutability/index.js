de2295d687c22f2e3eac4f5edc7ddc170cae2121
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-30 14:15:23 +0530

    Update index.js

--- 01_Basic JavaScript/34_Understand_String_Immutability/index.js
+++ 01_Basic JavaScript/34_Understand_String_Immutability/index.js
@@ -1,1 +1,9 @@
+// Setup
+let myStr = "Jello World";
 
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understand-string-immutability
