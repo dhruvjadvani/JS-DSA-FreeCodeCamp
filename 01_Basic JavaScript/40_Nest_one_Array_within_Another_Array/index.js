b2665a08f2746dd00f5f3aef0fd83da2d7075261
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-01 21:10:38 +0530

    Update index.js

--- 01_Basic JavaScript/40_Nest_one_Array_within_Another_Array/index.js
+++ 01_Basic JavaScript/40_Nest_one_Array_within_Another_Array/index.js
@@ -1,2 +1,5 @@
 // Only change code below this line
 const myArray = [['Dhruv', 23]];
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/nest-one-array-within-another-array
