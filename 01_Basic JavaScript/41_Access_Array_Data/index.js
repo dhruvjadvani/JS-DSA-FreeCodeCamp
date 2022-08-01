2a4df3c71e3e934c88027923ce9608acf7c3dc46
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-01 21:12:55 +0530

    Update index.js

--- 01_Basic JavaScript/41_Access_Array_Data/index.js
+++ 01_Basic JavaScript/41_Access_Array_Data/index.js
@@ -1,1 +1,4 @@
+const myArray = [50, 60, 70];
+let myData = myArray[0]; 
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/access-array-data-with-indexes
