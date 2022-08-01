a8c185326d71932738b5279a29c90c7251866bdb
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-01 21:08:36 +0530

    Update index.js

--- 01_Basic JavaScript/39_Store_Multiple_Values/index.js
+++ 01_Basic JavaScript/39_Store_Multiple_Values/index.js
@@ -3,4 +3,4 @@ const myArray = [];
 myArray[0] = "Dhruv";
 myArray[1] = 23;
 
-// 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/store-multiple-values-in-one-variable-using-javascript-arrays
