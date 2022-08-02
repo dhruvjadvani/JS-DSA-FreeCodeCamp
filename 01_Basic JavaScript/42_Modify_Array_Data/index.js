fb688bd62921ef231500c6faf142c658a22f1c20
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-02 23:07:18 +0530

    Update index.js

--- 01_Basic JavaScript/42_Modify_Array_Data/index.js
+++ 01_Basic JavaScript/42_Modify_Array_Data/index.js
@@ -1,1 +1,8 @@
+// Setup
+const myArray = [18, 64, 99];
 
+// Only change code below this line
+myArray[0] = 45;
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/modify-array-data-with-indexes
