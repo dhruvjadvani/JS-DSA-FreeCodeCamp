28ef69efe13e1ad3daa72721d764140e9277070b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-03 22:34:34 +0530

    Update index.js

--- 01_Basic JavaScript/45_pop()_in_arr/index.js
+++ 01_Basic JavaScript/45_pop()_in_arr/index.js
@@ -1,1 +1,8 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
 
+// Only change code below this line
+
+const removedFromMyArray = myArray.pop();
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-pop
