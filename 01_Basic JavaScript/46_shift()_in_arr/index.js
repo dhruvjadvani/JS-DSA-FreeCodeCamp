12b09bb5e934fc4d91679c638d120a491ffd46eb
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-03 22:42:37 +0530

    Update index.js

--- 01_Basic JavaScript/46_shift()_in_arr/index.js
+++ 01_Basic JavaScript/46_shift()_in_arr/index.js
@@ -1,1 +1,7 @@
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
 
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-shift
