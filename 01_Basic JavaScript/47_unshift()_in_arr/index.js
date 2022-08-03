7bf0abd2aeba443b9a0123705d748605dea1483a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-03 22:50:08 +0530

    Update index.js

--- 01_Basic JavaScript/47_unshift()_in_arr/index.js
+++ 01_Basic JavaScript/47_unshift()_in_arr/index.js
@@ -1,1 +1,8 @@
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
 
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-unshift
