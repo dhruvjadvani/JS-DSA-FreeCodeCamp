827f6f38e8b462de5435c1fe2d2ab06b8279e5ef
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-03 22:12:10 +0530

    Update index.js

--- 01_Basic JavaScript/44_push()_in_Arr/index.js
+++ 01_Basic JavaScript/44_push()_in_Arr/index.js
@@ -1,1 +1,7 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
 
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-push
