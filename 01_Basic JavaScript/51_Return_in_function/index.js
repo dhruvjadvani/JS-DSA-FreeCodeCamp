fc55468bc768cb2f04711d39e8a7726e0c768728
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-04 20:29:19 +0530

    Update index.js

--- 01_Basic JavaScript/51_Return_in_function/index.js
+++ 01_Basic JavaScript/51_Return_in_function/index.js
@@ -7,3 +7,6 @@ timesFive(5);
 timesFive(2);
 
 timesFive(0);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/return-a-value-from-a-function-with-return
