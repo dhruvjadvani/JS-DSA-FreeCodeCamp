f53af69cd6c5f45f1bb88fcacda4be0a96f7f66d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-04 20:26:38 +0530

    Update index.js

--- 01_Basic JavaScript/50_Functions_with_Arguments/index.js
+++ 01_Basic JavaScript/50_Functions_with_Arguments/index.js
@@ -7,3 +7,6 @@ console.log(sum1);
 
 let sum2 = functionWithArgs(7, 9);
 console.log(sum2);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/passing-values-to-functions-with-arguments
