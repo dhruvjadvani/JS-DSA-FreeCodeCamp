48f427bb498a422d0e3b5869f4219f4ba4fb67db
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-04 20:20:48 +0530

    Update index.js

--- 01_Basic JavaScript/49_Functions/index.js
+++ 01_Basic JavaScript/49_Functions/index.js
@@ -1,1 +1,7 @@
+function reusableFunction() {
+  console.log("Hi World");
+}
 
+reusableFunction();
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/write-reusable-javascript-with-functions
