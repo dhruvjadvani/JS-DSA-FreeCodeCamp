1089ea2b2434ba8c0ef43ebea95d7850c15593aa
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-06 23:33:34 +0530

    Update index.js

--- 01_Basic JavaScript/54_Global_vs._Local_Scope/index.js
+++ 01_Basic JavaScript/54_Global_vs._Local_Scope/index.js
@@ -1,1 +1,14 @@
+// Setup
+const outerWear = "T-Shirt";
 
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/global-vs--local-scope-in-functions
