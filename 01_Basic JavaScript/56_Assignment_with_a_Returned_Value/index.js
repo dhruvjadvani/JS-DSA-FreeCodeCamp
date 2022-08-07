f05d45bd9e78ee80831f4eeb378e3238bcdfdff0
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-07 23:00:26 +0530

    Update index.js

--- 01_Basic JavaScript/56_Assignment_with_a_Returned_Value/index.js
+++ 01_Basic JavaScript/56_Assignment_with_a_Returned_Value/index.js
@@ -1,1 +1,11 @@
+// Setup
+let processed = 0;
 
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/assignment-with-a-returned-value
