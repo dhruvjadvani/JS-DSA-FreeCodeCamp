be9122fcc361c85d80cd94878a0ba04ba0a5128d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-08 23:08:18 +0530

    Update index.js

--- 01_Basic JavaScript/60_Equality_Operator/index.js
+++ 01_Basic JavaScript/60_Equality_Operator/index.js
@@ -1,1 +1,11 @@
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
 
+testEqual(10);
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-equality-operator
