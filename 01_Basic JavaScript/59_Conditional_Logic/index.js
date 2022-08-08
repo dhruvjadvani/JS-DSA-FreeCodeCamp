5462ff4ce283e4fa7b173b84e395e8f722345d42
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-08 23:02:34 +0530

    Update index.js

--- 01_Basic JavaScript/59_Conditional_Logic/index.js
+++ 01_Basic JavaScript/59_Conditional_Logic/index.js
@@ -1,1 +1,14 @@
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue == true) {
+  return 'Yes, that was true';
+} else {
+  return 'No, that was false';
+}
 
+
+  // Only change code above this line
+
+}
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-conditional-logic-with-if-statements
