f8eb69abcba505967eda79a377d067c3e08ea3d5
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-09 22:55:04 +0530

    Update index.js

--- 01_Basic JavaScript/63_Comparison_with_the_Inequality_Operator/index.js
+++ 01_Basic JavaScript/63_Comparison_with_the_Inequality_Operator/index.js
@@ -1,1 +1,11 @@
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
 
+testNotEqual(10);
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-inequality-operator
