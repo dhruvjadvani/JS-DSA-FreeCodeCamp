26e5f6c9f15b1653f24086992330734b43fbe083
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-09 22:51:53 +0530

    Update index.js

--- 01_Basic JavaScript/62_Practice_comparing_different_values/index.js
+++ 01_Basic JavaScript/62_Practice_comparing_different_values/index.js
@@ -1,1 +1,12 @@
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
 
+compareEquality(10, "10");
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/practice-comparing-different-values
