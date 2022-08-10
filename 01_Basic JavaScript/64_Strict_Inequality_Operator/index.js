9780fc005631235bed6e645e8c8574ff9658cef9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-10 23:54:58 +0530

    Update index.js

--- 01_Basic JavaScript/64_Strict_Inequality_Operator/index.js
+++ 01_Basic JavaScript/64_Strict_Inequality_Operator/index.js
@@ -1,1 +1,12 @@
+// Setup
+function testStrictNotEqual(val) {
+  if (val) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
 
+testStrictNotEqual(10);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-strict-inequality-operator
