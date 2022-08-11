b1cd18c49906b764dd4a1cb563481f57a3d61a95
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-11 22:44:13 +0530

    Update index.js

--- 01_Basic JavaScript/65_Greater_Than_Operator/index.js
+++ 01_Basic JavaScript/65_Greater_Than_Operator/index.js
@@ -1,1 +1,16 @@
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
 
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-greater-than-operator
