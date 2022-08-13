fb67aba92ac86289379e39b798e36004317b3def
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-13 21:03:14 +0530

    Update index.js

--- 01_Basic JavaScript/69_Logical_And_Operator/index.js
+++ 01_Basic JavaScript/69_Logical_And_Operator/index.js
@@ -1,1 +1,15 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
 
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparisons-with-the-logical-and-operator
