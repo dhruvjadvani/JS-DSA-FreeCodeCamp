3e119af93df6518c9d45b82818f13196774e62f1
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-15 20:59:02 +0530

    Update index.js

--- 01_Basic JavaScript/74_Chaining_If_Else_Statements/index.js
+++ 01_Basic JavaScript/74_Chaining_If_Else_Statements/index.js
@@ -1,1 +1,22 @@
+function testSize(num) {
+  // Only change code below this line
+  if(num < 5) {
+    return "Tiny";
+  } else if(num < 10) {
+    return "Small";
+  } else if(num < 15) {
+    return "Medium";
+  } else if(num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
 
+ 
+  // Only change code above this line
+}
+
+testSize(7);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/chaining-if-else-statements
