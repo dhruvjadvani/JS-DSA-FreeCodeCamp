acc7e08667d4ccab2ca8030f7dedd69bca1fbb30
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-18 11:00:47 +0530

    Update index.js

--- 01_Basic JavaScript/79_Replacing_If_Else/index.js
+++ 01_Basic JavaScript/79_Replacing_If_Else/index.js
@@ -1,1 +1,32 @@
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
 
+  switch (val) {
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case "bob":
+      answer = "Marley";
+      break;
+    default:
+      answer = "";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/replacing-if-else-chains-with-switch
