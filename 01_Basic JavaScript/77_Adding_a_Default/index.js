4b59c3a78adae44c3975199c39d4caf93e0a1a8d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-18 10:44:30 +0530

    Update index.js

--- 01_Basic JavaScript/77_Adding_a_Default/index.js
+++ 01_Basic JavaScript/77_Adding_a_Default/index.js
@@ -1,1 +1,25 @@
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = 'apple';
+      break;
+    case 2:
+      answer = 'bird';
+      break;
+    case 3:
+      answer = 'cat';
+      break;
+    default:
+      answer = 'stuff';
+      break;
+    
+  }
 
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
