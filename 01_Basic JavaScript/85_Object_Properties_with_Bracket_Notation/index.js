8d1139ad8a3dac0a2e17b28f60506069179a60ae
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-19 21:09:31 +0530

    Update index.js

--- 01_Basic JavaScript/85_Object_Properties_with_Bracket_Notation/index.js
+++ 01_Basic JavaScript/85_Object_Properties_with_Bracket_Notation/index.js
@@ -1,1 +1,12 @@
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
 
+// Only change code below this line
+const entreeValue = testObj['an entree'];   // Change this line
+const drinkValue = testObj['the drink'];    // Change this line
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-object-properties-with-bracket-notation
