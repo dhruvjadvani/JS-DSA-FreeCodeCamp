3b3dfa831d24bafc66bcce295300942ab1999489
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-19 20:53:44 +0530

    Update index.js

--- 01_Basic JavaScript/82_Counting_Cards/index.js
+++ 01_Basic JavaScript/82_Counting_Cards/index.js
@@ -1,1 +1,28 @@
+let count = 0;
+let high = [2, 3, 4, 5, 6];
+let low = [10, 'J', 'Q', 'K', 'A'];
+function cc(card) {
+  // Only change code below this line
+  for(let i = 0; i < high.length; i++) {
+    if(high[i] == card) {
+        count++;
+    }
+  }
 
+  for(let i = 0; i < low.length; i++) {
+    if(low[i] == card) {
+        count--;
+    }
+  }
+
+  if(count > 0) {
+    return count + ' Bet';
+  } else {
+    return count + ' Hold';
+  }
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/counting-cards
