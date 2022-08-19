3e01a8d9462b711cc76089d1e4453ca451c20ae7
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-19 21:00:57 +0530

    Update index.js

--- 01_Basic JavaScript/83_Build_JavaScript_Objects/index.js
+++ 01_Basic JavaScript/83_Build_JavaScript_Objects/index.js
@@ -1,1 +1,10 @@
+const myDog = {
+  // Only change code below this line
+  name: 'John',
+  legs: 4,
+  tails: 1,
+  friends: ['a', 'b', 'c', 'd']
+  // Only change code above this line
+};
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/build-javascript-objects
