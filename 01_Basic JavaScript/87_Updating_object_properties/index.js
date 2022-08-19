c014330432fd8b36eaee8b8f6dcbf30a1198640d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-19 21:34:06 +0530

    Update index.js

--- 01_Basic JavaScript/87_Updating_object_properties/index.js
+++ 01_Basic JavaScript/87_Updating_object_properties/index.js
@@ -1,1 +1,12 @@
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
 
+// Only change code below this line
+myDog.name = 'Happy Coder';
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/updating-object-properties
