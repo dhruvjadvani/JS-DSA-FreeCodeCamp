7d00dae7b227474cf0618d58d8cc089b3b17064d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-19 21:37:08 +0530

    Update index.js

--- 01_Basic JavaScript/88_add_property_to_objects/index.js
+++ 01_Basic JavaScript/88_add_property_to_objects/index.js
@@ -1,1 +1,12 @@
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
 
+myDog.bark = 'bow-bow';
+
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/add-new-properties-to-a-javascript-object
