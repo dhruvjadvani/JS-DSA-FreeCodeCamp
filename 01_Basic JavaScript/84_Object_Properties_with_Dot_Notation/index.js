d28b3f8fe1acbffceb04dea2c9e0774e56359826
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-19 21:05:44 +0530

    Update index.js

--- 01_Basic JavaScript/84_Object_Properties_with_Dot_Notation/index.js
+++ 01_Basic JavaScript/84_Object_Properties_with_Dot_Notation/index.js
@@ -1,1 +1,12 @@
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
 
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-object-properties-with-dot-notation 
