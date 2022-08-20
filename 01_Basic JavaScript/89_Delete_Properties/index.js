5e372d6523e99d8ec7da44a99c1c48b4bc8a69ec
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-20 23:46:47 +0530

    Update index.js

--- 01_Basic JavaScript/89_Delete_Properties/index.js
+++ 01_Basic JavaScript/89_Delete_Properties/index.js
@@ -1,1 +1,14 @@
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
 
+// Only change code below this line
+delete myDog.tails;
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/delete-properties-from-a-javascript-object
