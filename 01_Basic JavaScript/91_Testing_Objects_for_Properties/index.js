84042999884adad9fb950837ffae779f4cd7bf2d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-21 14:54:06 +0530

    Update index.js

--- 01_Basic JavaScript/91_Testing_Objects_for_Properties/index.js
+++ 01_Basic JavaScript/91_Testing_Objects_for_Properties/index.js
@@ -1,1 +1,19 @@
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if(obj[checkProp] == undefined) {
+    return 'Not Found';
+  } else {
+    return obj[checkProp];
+  }
+  // Only change code above this line
+}
 
+checkObj.hasOwnProperty({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift");
+checkObj.hasOwnProperty({gift: "pony", pet: "kitten", bed: "sleigh"}, "pet");
+checkObj.hasOwnProperty({gift: "pony", pet: "kitten", bed: "sleigh"}, "house");
+checkObj.hasOwnProperty({city: "Seattle"}, "city");
+checkObj.hasOwnProperty({city: "Seattle"}, "district");
+checkObj.hasOwnProperty({pet: "kitten", bed: "sleigh"}, "gift");
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/testing-objects-for-properties
