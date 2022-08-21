70d73167f626c22bedba0d7d9c51edb2a11e75fa
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-21 14:41:39 +0530

    Update index.js

--- 01_Basic JavaScript/90_UsingObjectsForLookups/index.js
+++ 01_Basic JavaScript/90_UsingObjectsForLookups/index.js
@@ -1,1 +1,23 @@
+// Setup
+function phoneticLookup(val) {
+  let result = "";
 
+  let obj = {
+    "alpha": "Adams", 
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+     
+  result = obj[val];
+
+
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/using-objects-for-lookups
