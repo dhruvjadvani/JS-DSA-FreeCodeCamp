ade07be66c5c663804c525e416e6910e5f30e608
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-21 15:08:04 +0530

    Update index.js

--- 01_Basic JavaScript/92_Manipulating_Complex_Objects/index.js
+++ 01_Basic JavaScript/92_Manipulating_Complex_Objects/index.js
@@ -1,1 +1,26 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, 
+  {
+    "artist": "One Direction",
+    "title": "Up all night",
+    "release_year": 2011,
+    "formats": [
+      "CD",
+      "YT",
+      "Itunes"
+    ],
+    "gold": true
+  }
+];
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/manipulating-complex-objects
