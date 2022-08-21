ff38cd1e7d6d125ebdd93515efbc1eb798397754
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-21 15:26:12 +0530

    Update index.js

--- 01_Basic JavaScript/93_Accessing_Nested_Objects/index.js
+++ 01_Basic JavaScript/93_Accessing_Nested_Objects/index.js
@@ -1,1 +1,15 @@
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
 
+const gloveBoxContents = myStorage.car.inside['glove box'];
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-nested-objects
