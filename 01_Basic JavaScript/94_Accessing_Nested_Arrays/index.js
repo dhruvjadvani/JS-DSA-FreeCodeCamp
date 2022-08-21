48feedded4b6589157594b4c66bc53d574689bba
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-21 15:30:18 +0530

    Update index.js

--- 01_Basic JavaScript/94_Accessing_Nested_Arrays/index.js
+++ 01_Basic JavaScript/94_Accessing_Nested_Arrays/index.js
@@ -1,1 +1,23 @@
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
 
+const secondTree = myPlants[1].list[1];
+
+
+ // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-nested-arrays
