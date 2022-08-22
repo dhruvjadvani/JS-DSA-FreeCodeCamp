64c94d69ec85a60716680d1db8fe0e69e95e0b27
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-22 22:45:04 +0530

    Update index.js

--- 01_Basic JavaScript/100_Iterate_Through_an_Array/index.js
+++ 01_Basic JavaScript/100_Iterate_Through_an_Array/index.js
@@ -1,1 +1,10 @@
+// Setup
+const myArr = [2, 3, 4, 5, 6];
 
+// Only change code below this line
+let total = 0;
+for(let i = 0; i < myArr.length; i++) {
+  total = total + myArr[i];
+}
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-through-an-array-with-a-for-loop
