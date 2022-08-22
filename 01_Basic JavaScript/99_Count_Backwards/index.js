cd992399d16376276c195eb9602ca66cb47ad30a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-22 22:35:42 +0530

    Update index.js

--- 01_Basic JavaScript/99_Count_Backwards/index.js
+++ 01_Basic JavaScript/99_Count_Backwards/index.js
@@ -1,1 +1,12 @@
+// Setup
+const myArray = [];
+//let num = 9;
+// Only change code below this line
+for(let i = 10; i > 0; i--) {
+  if(i % 2 !== 0) {
+    myArray.push(i);
+  }
 
+}
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/count-backwards-with-a-for-loop
