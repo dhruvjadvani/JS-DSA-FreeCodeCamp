b797e4d59bff5cef07b362b033ffad02040e38ca
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-22 22:30:11 +0530

    Update index.js

--- 01_Basic JavaScript/98_Iterate_Odd_Numbers/index.js
+++ 01_Basic JavaScript/98_Iterate_Odd_Numbers/index.js
@@ -1,1 +1,10 @@
+// Setup
+const myArray = [];
+let num = 1;
+// Only change code below this line
+for(let i = 0; i < 5; i++) {
+  myArray.push(num);
+  num = num + 2;
+}
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-odd-numbers-with-a-for-loop
