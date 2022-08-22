6fe84c9096375ac23da8ced75fbe7b9c6be3074b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-22 22:25:14 +0530

    Update index.js

--- 01_Basic JavaScript/97_For_Loops/index.js
+++ 01_Basic JavaScript/97_For_Loops/index.js
@@ -1,1 +1,9 @@
+// Setup
+const myArray = [];
 
+// Only change code below this line
+for(let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-for-loops
