f907df01e6a249efea807ba49a28fa6c7cdf27aa
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-24 00:31:06 +0530

    Update index.js

--- 01_Basic JavaScript/993_Do...While_Loops/index.js
+++ 01_Basic JavaScript/993_Do...While_Loops/index.js
@@ -1,1 +1,13 @@
+// Setup
+const myArray = [];
+let i = 10;
 
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5) 
+  
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-do---while-loops
