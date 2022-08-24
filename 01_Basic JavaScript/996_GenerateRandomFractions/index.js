31b43bdef07c172b5a60a97a72a665a99e81d8ef
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-24 23:52:39 +0530

    Update index.js

--- 01_Basic JavaScript/996_GenerateRandomFractions/index.js
+++ 01_Basic JavaScript/996_GenerateRandomFractions/index.js
@@ -1,1 +1,11 @@
+function randomFraction() {
 
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-fractions-with-javascript
