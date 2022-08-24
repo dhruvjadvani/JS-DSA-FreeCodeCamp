12d3ac5771b0b4b32e407a3ceeb46a4f6e6baf41
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-24 23:56:14 +0530

    Update index.js

--- 01_Basic JavaScript/997_Generate_Random_Whole_Numbers/index.js
+++ 01_Basic JavaScript/997_Generate_Random_Whole_Numbers/index.js
@@ -1,1 +1,9 @@
+function randomWholeNum() {
 
+  // Only change code below this line
+  let randomNum = Math.floor(Math.random() * 10);
+
+  return randomNum;
+}
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-whole-numbers-with-javascript
