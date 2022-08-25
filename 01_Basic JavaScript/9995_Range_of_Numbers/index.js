0f6eb52f0cf625dc294fb41af839c547d4a38e76
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-26 00:01:25 +0530

    Update index.js

--- 01_Basic JavaScript/9995_Range_of_Numbers/index.js
+++ 01_Basic JavaScript/9995_Range_of_Numbers/index.js
@@ -1,1 +1,10 @@
+function rangeOfNumbers(startNum, endNum) {
+  if(endNum < startNum){
+    return [];
+  }
+  const rng = rangeOfNumbers(startNum, endNum - 1);
+  rng.push(endNum);
+  return rng;
+};
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-recursion-to-create-a-range-of-numbers
