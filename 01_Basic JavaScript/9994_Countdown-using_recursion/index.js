2b77eeb112cffd29a7cc01e776017124ceab3cb2
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-25 23:51:02 +0530

    Update index.js

--- 01_Basic JavaScript/9994_Countdown-using_recursion/index.js
+++ 01_Basic JavaScript/9994_Countdown-using_recursion/index.js
@@ -1,1 +1,15 @@
+// Only change code below this line
+function countdown(n){
+  if(n < 1) {
+    return []; 
+  }
 
+  const countArray = countdown(n - 1);
+  countArray.unshift(n);
+  return countArray;
+}
+// Only change code above this line
+countdown(5);
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-recursion-to-create-a-countdown
