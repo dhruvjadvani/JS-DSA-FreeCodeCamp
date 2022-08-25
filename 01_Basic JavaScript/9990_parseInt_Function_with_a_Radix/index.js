5db932ecda56c8e896220dc8bf08b4650f9212b4
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-25 23:13:24 +0530

    Update index.js

--- 01_Basic JavaScript/9990_parseInt_Function_with_a_Radix/index.js
+++ 01_Basic JavaScript/9990_parseInt_Function_with_a_Radix/index.js
@@ -1,1 +1,8 @@
+function convertToInteger(str) {
+  const binaryNum = parseInt(str, 2);
+  return binaryNum;
+}
 
+convertToInteger("10011");
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function-with-a-radix
