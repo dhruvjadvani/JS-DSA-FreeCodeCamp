882d118781c30a288ca34554b3ce4d176acf8460
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-25 23:05:14 +0530

    Update index.js

--- 01_Basic JavaScript/998_Whole_Numbers_within_a_Range/index.js
+++ 01_Basic JavaScript/998_Whole_Numbers_within_a_Range/index.js
@@ -1,1 +1,8 @@
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  let randomNum = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  return randomNum;
+  // Only change code above this line
+}
 
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/generate-random-whole-numbers-within-a-range
