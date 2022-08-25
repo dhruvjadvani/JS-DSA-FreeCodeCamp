57c714216cab8458f57bab70ba6a463a8d54631f
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-25 23:08:59 +0530

    Update index.js

--- 01_Basic JavaScript/999_parseInt_Function/index.js
+++ 01_Basic JavaScript/999_parseInt_Function/index.js
@@ -1,1 +1,8 @@
+function convertToInteger(str) {
+  const num = parseInt(str);
+  return num;
+}
 
+convertToInteger("56");
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function
