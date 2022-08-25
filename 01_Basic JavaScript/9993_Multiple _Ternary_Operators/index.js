a547cc6e6b10d2da49c6ae0068650b74e0ca6df9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-25 23:34:01 +0530

    Update index.js

--- 01_Basic JavaScript/9993_Multiple _Ternary_Operators/index.js
+++ 01_Basic JavaScript/9993_Multiple _Ternary_Operators/index.js
@@ -1,1 +1,7 @@
+function checkSign(num) {
+  return (num > 0) ? 'positive' : (num == 0) ? 'zero' : 'negative';
+}
 
+checkSign(10);
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-multiple-conditional-ternary-operators
