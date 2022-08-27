f762a74d5be60b5fd2635eaf885efa879f55cfa8
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-27 16:23:05 +0530

    Update index.js

--- 10_Projects/02_Roman_Numeral_Converter/index.js
+++ 10_Projects/02_Roman_Numeral_Converter/index.js
@@ -1,1 +1,32 @@
+function convertToRoman(num) {
+ let obj = {
+    M:  1000,
+    CM:	900,
+    D:	500,
+    CD:	400,
+    C:	100,
+    XC:	90,
+    L:	50,
+    XL:	40,
+    X:	10,
+    IX:	9,
+    V:	5,
+    IV:	4,
+    I:	1
+ }
 
+let romanNum = [];
+ if(num >= 10 && num < 40) {
+   if(num === 10) {
+     romanNum.push(num);
+   }
+   let lastDigit = num % 10;
+   romanNum.push(obj[lastDigit]);
+
+   let firstDigit = num / 10;
+   romanNum.unshift(obj[firstDigit]);
+ } else if()
+
+}
+
+convertToRoman(36);
