fd3866a41afaae2bc21e9650d713d35be2e106dc
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-27 16:10:59 +0530

    Update index.js

--- 10_Projects/01_Palindrome_checker/index.js
+++ 10_Projects/01_Palindrome_checker/index.js
@@ -1,1 +1,22 @@
+function palindrome(str) {
+  let revStr = "";
 
+  str = str. toLowerCase();
+  str = str.replace(/[\W_]/g,"");
+  
+  
+
+  for(let i = str.length - 1; i >= 0; i--) {
+    revStr = revStr + str.charAt(i);
+  }
+
+  if(str === revStr) {
+    return true;
+  } else {
+    return false;
+  }
+
+
+}
+
+palindrome("eye");
