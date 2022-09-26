a45b49cb28e91302187d0b7917742a185760d49d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-09-26 22:16:38 +0530

    Update index.js

--- 02_ES6/07_Use the Rest Parameter with Function Parameters/index.js
+++ 02_ES6/07_Use the Rest Parameter with Function Parameters/index.js
@@ -1,3 +1,8 @@
 const sum = (...args) => {
   return args.reduce((a, b) => a + b, 0);
 }
+
+// const sum = (x, y, z) => {
+//   const args = [x, y, z];
+//   return args.reduce((a, b) => a + b, 0);
+// }
