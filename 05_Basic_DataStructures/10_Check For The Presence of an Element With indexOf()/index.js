b3e3ec34f403a21683d71fea44e3d6423a1c8262
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-06 22:31:35 +0530

    Update index.js

--- 05_Basic_DataStructures/10_Check For The Presence of an Element With indexOf()/index.js
+++ 05_Basic_DataStructures/10_Check For The Presence of an Element With indexOf()/index.js
@@ -1,1 +1,13 @@
+function quickCheck(arr, elem) {
+  // Only change code below this line
+   let res = arr.indexOf(elem);
+    if(res === -1) {
+      return false;
+    } else {
+      return true;
+    }
 
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
