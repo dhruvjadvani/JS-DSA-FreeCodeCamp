4986a2ee5b36b1d9506d903cd919d9ff4bf089df
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-08 19:53:12 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/05_Return Largest Numbers in Arrays/index.js
+++ 06_Basic_Algorithm_Scripting/05_Return Largest Numbers in Arrays/index.js
@@ -1,1 +1,21 @@
+function largestOfFour(arr) {
+  let newArr = [];
+  
 
+  for(let i = 0; i < arr.length; i++) {
+    let bi = -Infinity;
+    for(let j = 0; j < arr[0].length; j++) {
+      if(bi < arr[i][j]) {
+        bi = arr[i][j]
+      }
+    }
+    newArr.push(bi);
+  }
+
+  // console.log(newArr);
+  return newArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
