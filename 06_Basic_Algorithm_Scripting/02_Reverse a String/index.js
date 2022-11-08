afe57392200061f28e61b75e6b00612634a76566
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-08 14:55:07 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/02_Reverse a String/index.js
+++ 06_Basic_Algorithm_Scripting/02_Reverse a String/index.js
@@ -1,1 +1,10 @@
+function reverseString(str) {
+  let newStr = "";
+  for(let i = str.length - 1; i >= 0; i--) {
+    newStr = newStr + str[i]; 
+  }
+  // console.log(newStr)
+  return newStr;
+}
 
+reverseString("hello");
