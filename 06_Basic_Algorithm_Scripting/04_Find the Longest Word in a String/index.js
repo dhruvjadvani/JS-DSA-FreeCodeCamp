2959a137ad30748138d112e32644e7d62dd2d518
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-08 17:43:20 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/04_Find the Longest Word in a String/index.js
+++ 06_Basic_Algorithm_Scripting/04_Find the Longest Word in a String/index.js
@@ -1,1 +1,14 @@
+function findLongestWordLength(str) {
+  let arr = str.split(" ");
+  // console.log(arr)
+  let len = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length >= len) {
+      len = arr[i].length;
+    }
+  }
+  // console.log(len)
+  return len;
+}
 
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
