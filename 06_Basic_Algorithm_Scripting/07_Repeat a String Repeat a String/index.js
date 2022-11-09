c4c8f2dfd3bfd67b4c62bd1511813b76a6d4cc68
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-09 09:58:39 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/07_Repeat a String Repeat a String/index.js
+++ 06_Basic_Algorithm_Scripting/07_Repeat a String Repeat a String/index.js
@@ -1,1 +1,10 @@
+function repeatStringNumTimes(str, num) {
+  let newStr = '';
+  for(let i = 0; i < num; i++) {
+    newStr += str;
+  }
+  // console.log(newStr)
+  return newStr
+}
 
+repeatStringNumTimes("abc", 3);
