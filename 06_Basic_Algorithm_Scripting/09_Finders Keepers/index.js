340cb7d5ca7331319b7246d5a0a00c6f260940d3
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-09 12:43:26 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/09_Finders Keepers/index.js
+++ 06_Basic_Algorithm_Scripting/09_Finders Keepers/index.js
@@ -1,1 +1,11 @@
+function findElement(arr, func) {
 
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0) {
+      return arr[i];
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
