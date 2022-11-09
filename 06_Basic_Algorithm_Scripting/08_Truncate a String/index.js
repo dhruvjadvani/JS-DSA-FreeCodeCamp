6383f0a14bc4b0acb0e9594d013120f258faed73
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-09 12:39:47 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/08_Truncate a String/index.js
+++ 06_Basic_Algorithm_Scripting/08_Truncate a String/index.js
@@ -1,1 +1,17 @@
+function truncateString(str, num) {
+  // let newStr = '';
+  // for(let i = 0; i < num; i++) {
+  //   newStr += str[i];
+  // }
+  // console.log(newStr +"...")
+  // return newStr + "...";
 
+  if(str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+  
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
