c246f351f79d33aab365da4df9d549a1bb74c991
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-09 09:56:00 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/06_Confirm the Ending/index.js
+++ 06_Basic_Algorithm_Scripting/06_Confirm the Ending/index.js
@@ -1,8 +1,18 @@
+// function confirmEnding(str, target) {
+//   if(str.endsWith(target) === true) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// confirmEnding("Bastian", "n");
+
+
 function confirmEnding(str, target) {
-  if(str.endsWith(target) === true) {
-    return true;
-  }
-  return false;
+  let re = new RegExp(target + "$", "i");
+
+  return re.test(str);
+
 }
 
 confirmEnding("Bastian", "n");
