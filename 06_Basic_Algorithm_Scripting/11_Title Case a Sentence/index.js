9cf86b403f38b24dcfeaf42530a3cad817f3ff4c
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-09 13:13:17 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/11_Title Case a Sentence/index.js
+++ 06_Basic_Algorithm_Scripting/11_Title Case a Sentence/index.js
@@ -1,1 +1,23 @@
+function titleCase(str) {
+  let newStr = '';
+  newStr = str.toLowerCase();
 
+  // console.log(newStr)
+
+  let arr = newStr.split(" ");
+  // console.log(arr);
+
+  let newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+      newArr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+
+  newArr = newArr.toString()
+  newArr = newArr.replace(/,/g, " ");
+
+  // console.log(newArr);
+  return newArr;
+}
+
+titleCase("I'm a little tea pot");
