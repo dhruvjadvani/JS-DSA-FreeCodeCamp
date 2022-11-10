86794d3d3c3ec6c3271ef1c651aa9fd5c4ef3dc0
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-10 13:09:15 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/16_Chunky Monkey/index.js
+++ 06_Basic_Algorithm_Scripting/16_Chunky Monkey/index.js
@@ -1,1 +1,23 @@
+function chunkArrayInGroups(arr, size) {
+  let temp = [];
+  const result = []
 
+  for(let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1){
+      temp.push(arr[i]);
+    } else {
+      temp.push(arr[i]);
+      result.push(temp);
+      temp = [];
+    }
+  }
+
+   if (temp.length !== 0) {
+     result.push(temp);
+   }
+
+   
+  return result;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
