f644df12836ffbe9553ae1a1f3b450fb1dd57de7
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-10 10:32:25 +0530

    Update index.js

--- 06_Basic_Algorithm_Scripting/14_Where do I Belong/index.js
+++ 06_Basic_Algorithm_Scripting/14_Where do I Belong/index.js
@@ -1,1 +1,15 @@
+function getIndexToIns(arr, num) {
+  // console.log(arr);
+  arr.push(num);
 
+  // console.log(arr);
+  arr.sort((a, b) => a - b);
+
+  // console.log(arr);
+  let idx = arr.indexOf(num);
+
+  // console.log(idx);
+  return idx;
+}
+
+getIndexToIns([40, 60], 50);
