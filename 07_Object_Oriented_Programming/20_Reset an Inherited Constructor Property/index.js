20d2f5ccef565c20acb016a9ace4f5debb7a02e2
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-11 13:41:32 +0530

    Update index.js

--- 07_Object_Oriented_Programming/20_Reset an Inherited Constructor Property/index.js
+++ 07_Object_Oriented_Programming/20_Reset an Inherited Constructor Property/index.js
@@ -1,1 +1,19 @@
+function Animal() {}
+function Bird() { }
+function Dog() { }
 
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// console.log(Bird.prototype)
+// Only change code below this line
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck = new Bird();
+duck.constructor
+let beagle = new Dog();
+beagle.constructor
+
+console.log(Bird.prototype)
