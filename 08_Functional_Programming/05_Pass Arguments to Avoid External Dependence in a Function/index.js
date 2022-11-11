4e2ef317c6186cf504639dff82e5740e06700d35
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-11 22:01:39 +0530

    Update index.js

--- 08_Functional_Programming/05_Pass Arguments to Avoid External Dependence in a Function/index.js
+++ 08_Functional_Programming/05_Pass Arguments to Avoid External Dependence in a Function/index.js
@@ -1,1 +1,10 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(val) {
+  let incr = val + 1;
+  return incr;
+  // Only change code above this line
+}
 
