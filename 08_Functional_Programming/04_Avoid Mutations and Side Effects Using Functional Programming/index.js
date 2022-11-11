740e3cba35e05633e751f5e602d2d143fb8a8841
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-11 21:56:40 +0530

    Update index.js

--- 08_Functional_Programming/04_Avoid Mutations and Side Effects Using Functional Programming/index.js
+++ 08_Functional_Programming/04_Avoid Mutations and Side Effects Using Functional Programming/index.js
@@ -3,7 +3,12 @@ let fixedValue = 4;
 
 function incrementer() {
   // Only change code below this line
-  return fixedValue + 1; 
+  let incr = fixedValue + 1; 
+  return incr;
+
 
   // Only change code above this line
 }
+
+incrementer();
+console.log(fixedValue);
