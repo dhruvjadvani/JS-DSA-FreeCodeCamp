b20fab9b68e891f142251ad8a94cf259c3d1d58d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-18 23:49:26 +0530

    Update index.js

--- 08_Functional_Programming/12_Remove Elements from an Array Using slice Instead of splice/index.js
+++ 08_Functional_Programming/12_Remove Elements from an Array Using slice Instead of splice/index.js
@@ -1,1 +1,14 @@
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  let res = cities.slice(0, 3);
+  // for(let i = 0; i < 3; i++) {
+  //   res.push(cities[i]);
+  // }
+  console.log(res);
+  return res;
 
+  // Only change code above this line
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
