3a9263b99635cb607f8c74d1d8096600c5fc2bb7
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-18 23:41:00 +0530

    Update index.js

--- 08_Functional_Programming/11_Return Part of an Array Using the slice Method/index.js
+++ 08_Functional_Programming/11_Return Part of an Array Using the slice Method/index.js
@@ -1,1 +1,11 @@
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
 
+  const res = anim.slice(beginSlice,endSlice);
+  return res;
+  
+  // Only change code above this line
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
