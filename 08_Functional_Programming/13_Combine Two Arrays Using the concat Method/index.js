3d37d0182d258588cf655ad3b65a31550716d197
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-19 00:05:39 +0530

    Update index.js

--- 08_Functional_Programming/13_Combine Two Arrays Using the concat Method/index.js
+++ 08_Functional_Programming/13_Combine Two Arrays Using the concat Method/index.js
@@ -1,1 +1,11 @@
+function nonMutatingConcat(original, attach) {
+  // Only change code below this line
+  const res = original.concat(attach);
+  return res;
 
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
