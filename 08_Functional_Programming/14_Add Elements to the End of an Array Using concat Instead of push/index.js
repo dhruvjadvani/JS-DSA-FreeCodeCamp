d3509d8b49cb84fb0932c8a01747ea8904f76fa9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-19 00:26:13 +0530

    Update index.js

--- 08_Functional_Programming/14_Add Elements to the End of an Array Using concat Instead of push/index.js
+++ 08_Functional_Programming/14_Add Elements to the End of an Array Using concat Instead of push/index.js
@@ -1,1 +1,11 @@
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  const res = original.concat(newItem);
+  return res;
 
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
