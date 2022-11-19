1f102166bfdd322f94f7eed556ca5525afec0280
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-19 12:24:22 +0530

    Update index.js

--- 08_Functional_Programming/22_Use the every Method to Check that Every Element in an Array Meets a Criteria/index.js
+++ 08_Functional_Programming/22_Use the every Method to Check that Every Element in an Array Meets a Criteria/index.js
@@ -1,1 +1,11 @@
+function checkPositive(arr) {
+  // Only change code below this line
+  const res = arr.every(function(val) {
+    return val > 0;
+  })
+  
+  return res;
+  // Only change code above this line
+}
 
+checkPositive([1, 2, 3, -4, 5]);
