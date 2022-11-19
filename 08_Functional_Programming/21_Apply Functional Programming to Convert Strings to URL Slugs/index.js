53d276d847100c5e2d599d46f79684cde49e2327
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-19 11:57:01 +0530

    Update index.js

--- 08_Functional_Programming/21_Apply Functional Programming to Convert Strings to URL Slugs/index.js
+++ 08_Functional_Programming/21_Apply Functional Programming to Convert Strings to URL Slugs/index.js
@@ -1,1 +1,12 @@
-
+// Only change code below this line
+function urlSlug(title) {
+  const lw = title.toLowerCase().trim();
+  console.log(lw);
+  const res = lw.split(/\s+/);
+  console.log(res);
+  const fin = res.join("-");
+  console.log(fin);
+  return fin;
+}
+// Only change code above this line
+urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
