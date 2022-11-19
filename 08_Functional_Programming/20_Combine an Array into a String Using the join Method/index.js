151fc2a57bde9b5a8466522e5d7a0778d6294f76
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-19 11:45:57 +0530

    Update index.js

--- 08_Functional_Programming/20_Combine an Array into a String Using the join Method/index.js
+++ 08_Functional_Programming/20_Combine an Array into a String Using the join Method/index.js
@@ -1,1 +1,12 @@
+function sentensify(str) {
+  // Only change code below this line
 
+  const res = str.split(/\W/);
+  console.log(res);
+  const newStr = res.join(" ");
+  console.log(newStr);
+  return newStr;
+  // Only change code above this line
+}
+
+sentensify("May-the-force-be-with-you");
