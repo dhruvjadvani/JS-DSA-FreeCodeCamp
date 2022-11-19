e34163f4f01f2c4341c20578e9c917c0f3120fda
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-19 10:57:24 +0530

    Update index.js

--- 08_Functional_Programming/18_Return a Sorted Array Without Changing the Original Array/index.js
+++ 08_Functional_Programming/18_Return a Sorted Array Without Changing the Original Array/index.js
@@ -1,1 +1,13 @@
+const globalArray = [5, 6, 3, 2, 9];
 
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  const newArr = arr.slice();
+  return newArr.sort(function(a, b) {
+    return a - b;
+  })
+
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
