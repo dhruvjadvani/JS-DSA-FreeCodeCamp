3129269de967eaabb75ca24b1f33f7d390ae4069
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-19 16:28:36 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/01_Sum All Numbers in a Range/index.js
+++ 09_Intermediate_Algorithm_Scripting/01_Sum All Numbers in a Range/index.js
@@ -1,1 +1,17 @@
+function sumAll(arr) {
+  const newArr = arr.sort(function(a,b) {
+    return a - b;
+  });
 
+  let sum = 0;
+  
+  for(let i = newArr[0]; i <= newArr[1]; i++) {
+    sum = sum + i;
+  }
+
+  console.log(sum);
+  return sum;
+ 
+}
+
+sumAll([1, 4]);
