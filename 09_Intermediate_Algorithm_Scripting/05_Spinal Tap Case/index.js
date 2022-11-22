2ca55bf2691bc2a536a230b94775e8bc2a97cd2d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-22 11:34:05 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/05_Spinal Tap Case/index.js
+++ 09_Intermediate_Algorithm_Scripting/05_Spinal Tap Case/index.js
@@ -1,1 +1,12 @@
+function spinalCase(str) {
+  
+  const arr = str.split(/\s|_|(?=[A-Z])/);
 
+  const strOne = arr.join("-");
+ 
+  const res = strOne.toLowerCase();
+  
+  return res;
+}
+
+spinalCase('This Is Spinal Tap');
