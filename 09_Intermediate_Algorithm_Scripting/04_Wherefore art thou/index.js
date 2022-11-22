2bbe02beabee9d5b0a5c06ffe06cdc7c72114833
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-22 11:22:51 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/04_Wherefore art thou/index.js
+++ 09_Intermediate_Algorithm_Scripting/04_Wherefore art thou/index.js
@@ -1,1 +1,20 @@
+function whatIsInAName(collection, source) {
+  const arr = [];
+  // Only change code below this line
+  for (let i = 0; i < collection.length; i++) {
+    let found = true;
+    for (const sourceProp in source) {
+      if (collection[i][sourceProp] !== source[sourceProp]) {
+        found = false;
+        break;
+      }
+    }
+    if (found) {
+      arr.push(collection[i]);
+    }
+  }
+  // Only change code above this line
+  return arr;
+}
 
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
