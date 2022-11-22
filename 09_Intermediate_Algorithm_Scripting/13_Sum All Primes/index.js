b9454e86f18be1006c7e3f300a1c652c1cb834a2
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-22 15:30:46 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/13_Sum All Primes/index.js
+++ 09_Intermediate_Algorithm_Scripting/13_Sum All Primes/index.js
@@ -1,1 +1,22 @@
+function sumPrimes(num) {
+  function isPrime(num) {
+    for(let div = 2; div * div <= num; div++) {
+      if(num % div === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
+  let sum = 0;
+  for(let i = 2; i <= num; i++) {
+    if(isPrime(i)) {
+      sum = sum + i;
+    }
+  }
+
+  return sum;
+
+}
+
+sumPrimes(10);
