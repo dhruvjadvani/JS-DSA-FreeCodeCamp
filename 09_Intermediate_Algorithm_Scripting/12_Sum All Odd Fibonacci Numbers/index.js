0f8f7abb22d8b4da9cf976e471733af0a48a276b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-22 15:05:38 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/12_Sum All Odd Fibonacci Numbers/index.js
+++ 09_Intermediate_Algorithm_Scripting/12_Sum All Odd Fibonacci Numbers/index.js
@@ -1,1 +1,16 @@
+function sumFibs(num) {
+  let a = 0;
+  let b = 1;
+  let sum = 0;
+  while (b <= num) {
+    if (b % 2 === 1) {
+      sum += b;
+    }
+    b += a;
+    a = b - a;
+  }
 
+  return sum;
+}
+
+sumFibs(4);
