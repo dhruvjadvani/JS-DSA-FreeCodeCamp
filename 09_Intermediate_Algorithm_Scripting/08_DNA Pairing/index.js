0c49e2386ab9ed4c0cfdf8817ae3a885b88cd387
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-22 13:02:33 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/08_DNA Pairing/index.js
+++ 09_Intermediate_Algorithm_Scripting/08_DNA Pairing/index.js
@@ -1,26 +1,18 @@
 function pairElement(str) {
-  
-  let matchWithBasePair = function(char, pairedArray) {
-    switch (char) {
-      case "A":
-        pairedArray.push(["A", "T"]);
-        break;
-      case "T":
-        pairedArray.push(["T", "A"]);
-        break;
-      case "C":
-        pairedArray.push(["C", "G"]);
-        break;
-      case "G":
-        pairedArray.push(["G", "C"]);
-        break;
-    }
-  };
 
-  
   const paired = [];
   for (let i = 0; i < str.length; i++) {
-    matchWithBasePair(str[i], paired);
+    
+    if(str[i] === 'A') {
+      paired.push(["A", "T"]);
+    } else if(str[i] === 'T') {
+      paired.push(["T", "A"]);
+    } else if(str[i] === 'C') {
+      paired.push(["C", "G"]);
+    } else if(str[i] === 'G') {
+      paired.push(["G", "C"]);
+    }
+    
   }
 
   return paired;
