a3e0e4a87852939cc1d972a129b11df473b43b14
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-22 12:09:31 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/07_Search and Replace/index.js
+++ 09_Intermediate_Algorithm_Scripting/07_Search and Replace/index.js
@@ -1,1 +1,17 @@
+function myReplace(str, before, after) {
+  let index = str.indexOf(before);
 
+  if (str[index] === str[index].toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  
+  str = str.replace(before, after);
+
+  return str;
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("Let us get back to more Coding", "Coding", "algorithms");
+myReplace("I think we should look up there", "up", "Down")
