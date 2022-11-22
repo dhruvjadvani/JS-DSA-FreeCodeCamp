ab1167a9b9011b8e53b8bc53c80e1f2c70d84c50
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-22 13:55:11 +0530

    Update index.js

--- 09_Intermediate_Algorithm_Scripting/10_Sorted Union/index.js
+++ 09_Intermediate_Algorithm_Scripting/10_Sorted Union/index.js
@@ -1,1 +1,22 @@
+function uniteUnique(arr) {
+  const finalArray = [];
 
+    for (let i = 0; i < arguments.length; i++) {
+      const arrayArguments = arguments[i];
+
+    
+        for (let j = 0; j < arrayArguments.length; j++) {
+          let indexValue = arrayArguments[j];
+
+          if (finalArray.indexOf(indexValue) < 0) {
+            finalArray.push(indexValue);
+          }
+          
+        }
+    }
+  
+  console.log(finalArray);
+  return finalArray;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
