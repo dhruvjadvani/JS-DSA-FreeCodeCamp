511621311b6cb5f6eaf045bdacc14a0d7f2f3ecc
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-05-26 05:51:43 +0530

    Update index.js

--- 10_Projects/04_Telephone Number Validator/index.js
+++ 10_Projects/04_Telephone Number Validator/index.js
@@ -1,1 +1,13 @@
+function telephoneCheck(str) {
+  // Regular expression pattern to match the valid US phone number formats
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?(\d{3})[-\s]?(\d{4})$/;
+  
+  // Test the input string against the regex pattern
+  return regex.test(str);
+}
+
+console.log(telephoneCheck("555-555-5555")); // true
+console.log(telephoneCheck("(555)555-5555")); // true
+console.log(telephoneCheck("(555) 555-5555")); // true
+
 
