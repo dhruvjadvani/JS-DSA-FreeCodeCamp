738c303cfe2b5d7f00858e60031dbcfde28fb434
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2023-05-26 05:46:28 +0530

    Update index.js

--- 10_Projects/03_Caesars Cipher/index.js
+++ 10_Projects/03_Caesars Cipher/index.js
@@ -1,1 +1,19 @@
+function rot13(str) {
+  let decodedMessage = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (/[A-Z]/.test(char)) {
+      let decodedChar = String.fromCharCode((char.charCodeAt(0) - 65 + 13) % 26 + 65);
+      decodedMessage += decodedChar;
+    } else {
+      decodedMessage += char;
+    }
+  }
+
+  return decodedMessage;
+}
+
+console.log(rot13("SERR PBQR PNZC")); 
 
